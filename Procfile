309579ca8393b4c986784ea3f5848aecd1de082e
braulefernandes <braulefernandes@yahoo.com>
2024-12-01 13:51:36 -0300

    Tentativa de conexão Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn relatorioGeral.wsgi:application
--- relatorioGeral/settings.py
+++ relatorioGeral/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dj_database_url import parse as db_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-nyxjtjdz^$@ig7^j=1mp9j^g-84hbbff*d7z*og+-^9@l%(oqx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,16 +77,8 @@ WSGI_APPLICATION = 'relatorioGeral.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bd_relatorio',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': db_url(os.getenv('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
